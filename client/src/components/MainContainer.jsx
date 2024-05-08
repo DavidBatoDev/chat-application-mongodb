b05cf498e2d12be4fe0f54b66ee04271eec1d16a
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-05-08 17:24:11 +0800

    added taiwind and a container

---
+++ client/src/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContainer = () => {
+  return (
+    <div className='bg h-[90vh] w-[90vw] bg-white rounded-xl'>
+      Yow
+    </div>
+  )
+}
+
+export default MainContainer
