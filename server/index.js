189505520285afc804d707ea637846416632a4e2
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-05-13 19:04:23 +0800

    complete database connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
+// imports
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-const app = express();
+// constants
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5002;
+const DB_URI = process.env.MONGODB_URI;
 
-const PORT = process.env.PORT
+// middlewares
+mongoose.connect(DB_URI).then(() => {
+    console.log('Database connected');
+}).catch((error) => {
+    console.log('Error connecting to database', error);
+});
 
 app.use('/', (req, res) => {
-    res.send('API is working');
+    res.send('Hello World');
 })
 
+// listen
 app.listen(PORT, () => {
     console.log('Server is running on port ', PORT);
 })
