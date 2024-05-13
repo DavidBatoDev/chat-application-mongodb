9f360999efe2bfa87e6db33b864f7a0bac007500
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-05-13 21:53:21 +0800

    fix user model bug

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -7,6 +7,7 @@ export const login = (req, res) => {
         const user = User.findOne({email});
         if (!user) {
             res.status(400).json({error: 'User not found'});
+            return;
         }
         const passwordIsValid = bcrypt.compareSync(password, user.password);
         if (!passwordIsValid) {
@@ -21,18 +22,18 @@ export const login = (req, res) => {
 
 export const register = async (req, res) => {
     const {name, email, password} = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-    })
     try {
-        const user = User.findOne({email})
-        if (user) {
-            res.status(400).json({error: 'User already exists'});
+        const userExist = await User.findOne({email});
+        if (userExist) {
+            return res.status(400).json({error: 'User already exists'});
         }
-        await newUser.save();
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+        })
+        await user.save();
         res.status(201).json({message: 'User added'});
     } catch (error) {
         res.status(400).json({error});
