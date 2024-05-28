303ce0fe50eae1e33735d33871d620ce9dadc686
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-05-29 00:20:23 +0800

    add query param to fetch available users and groups

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,12 +1,36 @@
 import User from '../models/user-model.js'
+import Chat, {GroupChat} from '../models/chat-model.js'
 
-export const fetchAllUsers = async (req, res) => {
+export const fetchUsers = async (req, res) => {
     try {
         if (!req.user) {
             return res.status(401).json({message: 'Unauthorized'});
         }
-        const users = await User.find({ _id: { $ne: req.user._id } });
+        const search = req.query.search || '';
+        let users;
+        if (search === '') {
+            users = await User.find({ _id: { $ne: req.user._id } });
+        } else {
+            users = await User.find({ name: { $regex: search, $options: 'i' }, _id: { $ne: req.user._id } });
+        }
         res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const fetchGroups = async (req, res) => {
+    try {
+        if (!req.user) {
+            return res.status(401).json({message: 'Unauthorized'});
+        }
+        const search = req.query.search || '';
+        if (search !== '') {
+            const groups = await Chat.find({isGroupChat: true, chatName: { $regex: search, $options: 'i' }});
+            return res.status(200).json(groups);
+        }
+        const groups = await GroupChat.find({users: req.user._id}).populate('users');
+        res.status(200).json(groups);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import {
-    fetchAllUsers
+    fetchUsers,
+    fetchGroups
 } from '../controllers/user-controller.js';
 import { authorizationHandler } from '../utils/authorizationHandler.js'
 
 const router = express.Router();
 
-router.get('/fetchUsers', authorizationHandler, fetchAllUsers);
+router.get('/fetchUsers', authorizationHandler, fetchUsers);
+
+router.get('/fetchGroups', authorizationHandler, fetchGroups);
 
 export default router;
